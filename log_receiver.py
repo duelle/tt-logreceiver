8a2639b5e6503794ed294d69746beb1bc180fb39
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-01-30 14:26:18 +0100

    Changed log-format to CSV.

--- log_receiver.py
+++ log_receiver.py
@@ -4,73 +4,74 @@ import json
 import datetime
 import time
 import urllib.request
-
+import tempfile
+import os
 
 app = Flask(__name__)
+tempdir = tempfile.mkdtemp(prefix='tt_logreceiver_')
+print('tempdir is: ' + tempdir)
+log_header = 'uuid;datetime_triggered;datetime_started;datetime_finished;build_id;job_id;startup_duration;build_url;job_url'
 
-@app.route("/build/triggered/", methods=['POST'])
+@app.route('/build/triggered/', methods=['POST'])
 def build_triggered():
   now = datetime.datetime.now()
   build_uuid = request.form['build_uuid'].strip()
 
-  line = str(now) + ";" + "triggered" + '\n'
+  line = str(now) + 'triggered'
 
-  with open('/tmp/lr_' + build_uuid + '.log', "a") as log_file:
-    log_file.write(str(build_uuid) + '\n')
-    log_file.write(line)
+  with open(tempdir + os.sep + build_uuid + '.csv', 'a') as log_file:
+    log_file.write(log_header + '\n')
+    log_file.write(str(build_uuid) + ';"' + str(now) + '";')
 
   return line, status.HTTP_200_OK
 
 
-@app.route("/job/started/", methods=['POST'])
+@app.route('/job/started/', methods=['POST'])
 def job_started():
   now = datetime.datetime.now()
 
   build_uuid = request.form['build_uuid'].strip()
-  build_id = request.form['build_id'].strip()
-  build_web_url = request.form['build_web_url'].strip()
 
-  job_id = request.form['job_id'].strip()
-  job_web_url = request.form['build_web_url'].strip()
-
-  line = str(now) + ";" \
-           + "started" + ";" \
-           + str(build_id) + ";" \
-           + str(build_web_url) + ";" \
-           + str(job_id) + ";" \
-           + str(job_web_url) + ";" \
-           + '\n'
-
-  with open('/tmp/lr_' + build_uuid + '.log', "a") as log_file:
-    log_file.write(line)
+  with open(tempdir + os.sep + build_uuid + '.csv', 'a') as log_file:
+    log_file.write('"' + str(now) + '";')
 
   return line, status.HTTP_200_OK
 
-@app.route("/job/finished/", methods=['POST'])
+
+@app.route('/job/finished/', methods=['POST'])
 def job_finished():
   now = datetime.datetime.now()
 
   build_uuid = request.form['build_uuid'].strip()
   build_id = request.form['build_id'].strip()
   build_web_url = request.form['build_web_url'].strip()
-
   job_id = request.form['job_id'].strip()
   job_web_url = request.form['build_web_url'].strip()
 
-  line = str(now) + ";" \
-           + "finished" + ";" \
-           + str(build_id) + ";" \
-           + str(build_web_url) + ";" \
-           + str(job_id) + ";" \
-           + str(job_web_url) + ";" \
+  time.sleep(10)
+
+  tt_logfile = tempdir + os.sep + build_uuid + '-' + build_id + '-' + job_id + '.log'
+
+  urllib.request.urlretrieve('https://api.travis-ci.com/v3/job/' + job_id + '/log.txt', tt_logfile)
+
+  startup_duration = ''
+  with open(tt_logfile, 'r') as ttlog:
+    for line in ttlog:
+      if line.startswith('startup:'):
+        startup_duration = line.split(':')[1].strip()
+        break;
+
+
+  line = '"' + str(now) + '";' \
+           + str(build_id) + ';' \
+           + str(job_id) + ';' \
+           + '"' + str(startup_duration) + '";' \
+           + '"' + str(build_web_url) + '";' \
+           + '"' + str(job_web_url) + '";' \
            + '\n'
 
-  with open('/tmp/lr_' + build_uuid + '.log', "a") as log_file:
+  with open(tempdir + os.sep + build_uuid + '.csv', 'a') as log_file:
     log_file.write(line)
-
-  time.sleep(30)
-
-  urllib.request.urlretrieve('https://api.travis-ci.com/v3/job/' + job_id + '/log.txt', '/tmp/lr_tr_' + build_uuid + '-' + build_id + '-' + job_id + '.log')
 
   return line, status.HTTP_200_OK
 
